9b6b57d54d056e57a0e303cb000e8fc57754d529
ProgramerNinja <nohamcmjt@gmail.com>
2023-11-29 20:52:32 -0700

    Added the database and supporting files

---
+++ models/Children.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Child extends Model {}
+
+Child.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    child_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },    
+    last_inital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    naughtyNice: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'child',
+  }
+);
+
+module.exports = Child;
+
