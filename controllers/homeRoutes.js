78e1810942150e5d71228f101cc01c94a6ec63a8
SlemJosh <joshua.slem@gmail.com>
2023-12-03 17:08:48 -0700

    Added lists to the directory.  Also changed homepage on / to main.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,13 +1,16 @@
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
-    res.render('homepage', {});
+    res.render('main', {});
 });
 router.get('/login', async (req, res) => {
     res.render('login', {});
 });
 router.get('/profile', async (req, res) => {
     res.render('profile', {});
+});
+router.get('/lists', async (req, res) => {
+    res.render('lists', {});
 });
 
 
