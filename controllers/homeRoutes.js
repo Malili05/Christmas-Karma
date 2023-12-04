39e73a64b2907d0d1d5de4f761fc865bda6c9c49
SlemJosh <joshua.slem@gmail.com>
2023-12-03 19:36:40 -0700

    Changed the background mp4.  Added a lists routing, as well as moved some content over to the homepage handlebar.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
-    res.render('main', {});
+    res.render('homepage', {});
 });
 router.get('/login', async (req, res) => {
     res.render('login', {});
