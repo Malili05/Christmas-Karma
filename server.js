9f1edd05ef3fe7df40141cba5ea99a8059602361
Malili05 <malisalitten@gmail.com>
2023-12-04 20:27:19 -0700

    added image

--- server.js
+++ server.js
@@ -36,6 +36,8 @@ const sess = {
 
 app.use(session(sess));
 
+app.use(express.static(path.join(__dirname,'public')));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
