2de6a96c1ea971abf0608c949bd28df7212c825a
ProgramerNinja <nohamcmjt@gmail.com>
2023-12-06 21:43:51 -0700

    fixed chart

--- controllers/api/childRoutes.js
+++ controllers/api/childRoutes.js
@@ -63,4 +63,23 @@ router.delete('/:id', apiAuth, async (req, res) => {
   }
 });
 
+router.get('/chartData', async (req, res) => {
+  var data = {
+    labels: ['Nice', 'Naughty'],
+    datasets: [{
+        data: [], // Example data, replace with your actual data
+        backgroundColor: ['#36A2EB', '#FF6384'],
+        hoverBackgroundColor: ['#36A2EB', '#FF6384'],
+    }],
+};
+const db_data= await Child.findAll({})
+let naughty= 0, nice= 0;
+db_data.forEach(record => {
+  if(record.childNiceness) naughty+= 1
+  else nice += 1
+})
+data.datasets[0].data.push(naughty, nice)
+  res.send(data)
+});
+
 module.exports = router;
--- controllers/chartRoutes.js
+++ controllers/chartRoutes.js
@@ -10,22 +10,4 @@ router.get('/', (req, res) => {
   res.render('chart');
 });
 
-router.get('/chart/test', async (req, res) => {
-  var data = {
-    labels: ['Nice', 'Naughty'],
-    datasets: [{
-        data: [], // Example data, replace with your actual data
-        backgroundColor: ['#36A2EB', '#FF6384'],
-        hoverBackgroundColor: ['#36A2EB', '#FF6384'],
-    }],
-};
-const db_data= await Child.findAll({})
-let naughty= 0, nice= 0;
-db_data.forEach(record => {
-  if(record.childNiceness) naughty+= 1
-  else nice += 1
-})
-data.datasets[0].data.push(naughty, nice)
-  res.send(data)
-});
 module.exports = router;
